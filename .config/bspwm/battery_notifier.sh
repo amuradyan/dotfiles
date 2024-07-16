2ca28f98c8e7884f63867147d168dc045a29a5db
darthspectrum <muradianator@gmail.com>
2024-07-16 17:51:03 +0400

    chore: latest configs from work machine

--- .config/bspwm/battery_notifier.sh
+++ .config/bspwm/battery_notifier.sh
@@ -2,17 +2,42 @@
 
 BATTERY_PATH="/sys/class/power_supply/BAT0"
 
-send_notification() {
-  level=$1
-  urgency=$2
-  bgcolor=$3
-  fgcolor=$4
+FYI=1
+WORRYING=2
+CRITICAL=3
 
-  dunstify \
-    -u "$urgency" \
-    --hints=string:bgcolor:"$bgcolor" \
-    --hints=string:fgcolor:"$fgcolor" \
-    "Battery Warning" "Battery level is at ${level}%!"
+get_notification_params() {
+  id=$1
+
+  case $id in
+    $FYI)
+      echo "low #006600 #ffffff"
+      ;;
+    $WORRYING)
+      echo "normal #ffff00 #000000"
+      ;;
+    $CRITICAL)
+      echo "critical #993333 #000000"
+      ;;
+    *)
+      echo "Invalid ID for notification"
+      ;;
+  esac
+}
+
+send_notification() {
+  id=$1
+  read urgency bgcolor fgcolor <<< $(get_notification_params "$id")
+  current_charge=$(cat "$BATTERY_PATH/capacity")
+
+  if [ "$urgency" != "Invalid ID for notification" ]; then
+    dunstify \
+      -u "$urgency" \
+      --hints=string:bgcolor:"$bgcolor" \
+      --hints=string:fgcolor:"$fgcolor" \
+      -r "$id" \
+      "Battery Warning" "Battery level is at ${current_charge}%!"
+  fi
 }
 
 previous_charge=-1
@@ -22,11 +47,11 @@ while true; do
 
   if [ "$current_charge" -lt "$previous_charge" ]; then
     if [ "$current_charge" -le 5 ] && [ "$current_charge" -ge 2 ]; then
-      send_notification "$current_charge" critical "#993333" "#000000"
+      send_notification "$CRITICAL"
     elif [ "$current_charge" -eq 10 ]; then
-      send_notification "$current_charge" normal "#ffff00" "#000000"
+      send_notification "$WORRYING"
     elif [ "$current_charge" -eq 15 ]; then
-      send_notification "$current_charge" low "#006600" "#ffffff"
+      send_notification "$FYI"
     fi
   fi
 
--- Readme.md
+++ Readme.md
@@ -10,7 +10,7 @@ This repo contains settings for different tools (a la dotfiles)
 
 ## Stuff I install
 
-> sudo apt install mc tmux tmuxinator tig zsh iwd xsecurelock rofi polybar dunst
+> sudo apt install mc tmux tmuxinator tig zsh iwd xsecurelock rofi polybar dunst scrot
 
 ## Awesome oldschool fonts everybody needs
 
