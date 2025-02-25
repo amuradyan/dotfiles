96d6b9f1c498bba29298dc3e1d567f102c76936e
darthspectrum <muradianator@gmail.com>
2025-02-25 04:03:46 +0400

    chore: organizing monitors when external HDMI is connected

--- .config/bspwm/display_hdmi.sh
+++ .config/bspwm/display_hdmi.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-xrandr --output eDP-1 --primary --mode 1920x1080 \
-       --output HDMI-1 --auto --mode 1920x1080 --above eDP-1 \
-       --output DP-1 --off
+xrandr --output eDP-1 --auto  \
+       --output HDMI-1-0 --primary --auto --above eDP-1
 
-bspc monitor HDMI-1 -d I II III IV V VI
-bspc monitor eDP-1 -d VII VIII IX X
+bspc wm --reorder-monitors eDP-1 HDMI-1-0
+
+bspc monitor eDP-1 -d I II III IV
+bspc monitor HDMI-1-0 -d V VI VII VIII IX X
